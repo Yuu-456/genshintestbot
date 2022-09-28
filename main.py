58e5947eb667e9f4c72804fdcfd91b47649dabaa
customfuck <100310269+customfuck@users.noreply.github.com>
2022-09-28 14:15:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5589,11 +5589,11 @@ async def start(event):
     elif command in Oganesson:
         velement = 'oganesson'
     file = perodic_table[velement]["bohr_model_image"]
-    await client.send_file(event.sender_id, file = file, caption = "`Atomic Name: "+str(perodic_table[velement]['name'])+"\nAtomic Number: "+str(perodic_table[velement]['number'])+"\nSymbol: "+str(perodic_table[velement]['symbol'])+"\nAtomic Mass: "+str(perodic_table[velement]['atomic_mass'])+"\nMelting: "+str(perodic_table[velement]['melt'])+"\nBoiling: "+str(perodic_table[velement]['boil'])+"\nDensity: "+str(perodic_table[velement]['density'])+"\nMolar Heat: "+str(perodic_table[velement]['molar_heat'])+"\nPeriod: "+str(perodic_table[velement]['period'])+"\nPhase: "+str(perodic_table[velement]['phase'])+"`"
+    await client.send_file(event.sender_id, file = file, caption = "`Atomic Name: "+str(perodic_table[velement]['name'])+"\nAtomic Number: "+str(perodic_table[velement]['number'])+"\nSymbol: "+str(perodic_table[velement]['symbol'])+"\nAtomic Mass: "+str(perodic_table[velement]['atomic_mass'])+"\nMelting: "+str(perodic_table[velement]['melt'])+"\nBoiling: "+str(perodic_table[velement]['boil'])+"\nDensity: "+str(perodic_table[velement]['density'])+"\nMolar Heat: "+str(perodic_table[velement]['molar_heat'])+"\nPeriod: "+str(perodic_table[velement]['period'])+"\nPhase: "+str(perodic_table[velement]['phase'])+"\nCategory: "+str(perodic_table[velement]['category'])+"\nShells: "+str(perodic_table[velement]['shells'])+"\nConfig: "+str(perodic_table[velement]['electron_configuration'])
         ,
         buttons=[
         [
-            Button.url("More about"+perodic_table[velement]['name'], perodic_table[velement]['source'])
+            Button.url("More about "+perodic_table[velement]['name'], perodic_table[velement]['source'])
         ]
      ]
     )
