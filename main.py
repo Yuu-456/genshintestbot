88b80aa47644c8a1127d8b441d3b70e287034e38
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-10-21 11:43:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,24 @@
 import os
-
 from telethon.tl.functions.channels import GetParticipantsRequest
-
 from telethon.tl.types import ChannelParticipantsSearch
-
 from time import sleep
-
 from telethon.tl.types import MessageEntityCode
-
 from telethon import TelegramClient, events, Button
-
 import telethon.sync #lol copied from docs
-
 import asyncio
-
 import logging
-
 import asyncio
-
 from telethon import events
-
 from telethon.errors import UserNotParticipantError
-
 from telethon.tl.functions.channels import GetParticipantRequest
-
 from telethon.tl.types import ChannelParticipantAdmin
-
 from telethon.tl.types import ChannelParticipantCreator
 
 API_ID = os.environ.get('API_ID', None)
-
 API_HASH = os.environ.get('API_HASH', None)
-
 TOKEN = os.environ.get('TOKEN', None)
-
 api_id = API_ID
-
 api_hash = API_HASH
-
 bot_token = TOKEN
 
 client = TelegramClient('aucbout', api_id, api_hash).start(bot_token=bot_token) #i dont really understand it lol but without this bot wont work
@@ -51,5 +32,4 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 client.start()
-
 client.run_until_disconnected()
