b579a95db5581cf0751abbfb55c47a1565de1cf6
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-10-23 06:57:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def handler(event):
     user_level = (f"Level: {user.player.level}")
     user_worldlevel = (f'World level:{user.player.worldLevel}')
     user_abyss = (f'Abyss: {user.player.towerFloorIndex}-{user.player.towerLevelIndex}')
-    await event.reply("Nickname : {}\nAdventer Rank : {}\nWorld_level : {}\nAbyss : {}".format(user_nickname, user_level, user_worldlevel, user_abyss)
+    await event.reply("Nickname : {}\nAdventer Rank : {}\nWorld_level : {}\nAbyss : {}".format(user_nickname, user_level, user_worldlevel, user_abyss))
 
 @client.on(events.NewMessage(pattern='/mycharacter'))
 async def handler(event):
